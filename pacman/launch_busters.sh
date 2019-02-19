f17294fac80d745b6ec24966d7e888cb30687c4d
CarlosUni <100074907@alumnos.uc3m.es>
2019-02-19 19:01:10 +0100

    Mejorado mapa para retener a los fantasmas comidos

--- pacman/launch_busters.sh
+++ pacman/launch_busters.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Lanzador de Pac-Man modo busters en el mapa oneHunt
 echo Ejecutando pacman-busters
-python busters.py -n 1 -l custom_catedral -p BustersKeyboardAgent -g RandomGhost -k 2 -t 0.25
+python busters.py -n 1 -l customCatedral -p BustersKeyboardAgent -g RandomGhost -k 4 -t 0.17
+#python busters.py -n 1 -l customCatedral -p BustersKeyboardAgent -g RandomGhost -k 4 -t 0.17
