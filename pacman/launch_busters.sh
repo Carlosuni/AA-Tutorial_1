ae41d0ee74bc0d45411994b7196f64894f6e0087
CarlosUni <100074907@alumnos.uc3m.es>
2019-02-19 21:12:04 +0100

    Añadido BasiAgentAA

--- pacman/launch_busters.sh
+++ pacman/launch_busters.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Lanzador de Pac-Man modo busters en el mapa oneHunt
 echo Ejecutando pacman-busters
-python busters.py -n 1 -l customCatedral -p BustersKeyboardAgent -g RandomGhost -k 4 -t 0.17
-#python busters.py -n 1 -l customCatedral -p BustersKeyboardAgent -g RandomGhost -k 4 -t 0.17
+#python busters.py -n 1 -l customCatedral -p BustersKeyboardAgent -g RandomGhost -k 4 -t 0.25
+
+#python busters.py -p BasicAgentAA
+
+
